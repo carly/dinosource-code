157aeaf1f71661a2e2c55d837012a0381309bf22
carobinson <carly.a.robinson@gmail.com>
2015-09-28 15:33:46 -0700

    got source code to return. working on pprint and helper functions.

--- server.py
+++ server.py
@@ -1,15 +1,17 @@
 """dinosource code"""
 
 #imports - standard lib
+import lxml.html
 import os
-import pprint
+import re
+import requests
 
 #imports - flask
 from flask import Flask, render_template, redirect, request, flash, session, jsonify, url_for
-from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
 
 #imports - 3rd party
+from bs4 import BeautifulSoup
 from jinja2 import StrictUndefined
 from werkzeug import secure_filename
 
@@ -18,8 +20,6 @@ from werkzeug import secure_filename
 
 #app config
 app = Flask(__name__)
-
-printer = pprint.PrettyPrinter()
 
 app.secret_key="""SuperSecret"""
 app.jinja_env.undefined = StrictUndefined
@@ -31,16 +31,45 @@ def index():
 	
 	return render_template('index.html')
 
-@app.route('/source-code')
-def source_code():
-	"""renders view with source code and tag summary."""
+
+@app.route('/parse-url', methods=["GET", "POST"])
+def parse_url():
+	"""tbd"""
 	
-	return render_template('dinosource.html')
+	if request.method == "POST":
+		# get url that person has entered
+		try: 
+			url = request.form.get("parse-url")
+			r = requests.get(url)
+			
+		except (requests.exceptions.ConnectionError, requests.exceptions.InvalidURL):
+			flash('Roar. Invalid URL or Connection Error. Try again plz.')
+			return redirect('/')
+		
+		if r: 
+			
+			# use beautiful soup to parse
+			raw_html = BeautifulSoup(r.text)
+			source_code = raw_html.prettify(formatter=None)
+			
+			# dictionary to track tag frequency
+			tag_dict = {}
+			i = 1
+			for tag in raw_html.find_all(True):
+				if tag.name not in tag_dict.keys():
+					tag_dict[tag.name] = i 
+				else: 
+					tag_dict[tag.name] = tag_dict[tag.name] + 1
+					
+			print tag_dict
+			
+			
+	return render_template('dinosource.html', source_code=source_code)
+
 
 if __name__ == "__main__":
 	#debug=True for DebugToolbarExtension to work
 	app.debug = True
-	connect_to_db(app)
 
 	#Use the DebugToolbar
 	DebugToolbarExtension(app)
